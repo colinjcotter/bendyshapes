a2a362f0f95867b675ea9487db5014368c5713fd
Colin Cotter <colin.cotter@imperial.ac.uk>
2018-02-24 14:36:13 +0000

    curvature flow works

--- compressive.py
+++ compressive.py
@@ -2,41 +2,51 @@ from firedrake import *
 Lx = 0.1
 Ly = 10.
 
-nx = 5
-ny = 100
+ref_level = 3
+deg = 2
+mesh = UnitIcosahedralSphereMesh(ref_level, degree=deg)
+Xn = mesh.coordinates
+V = VectorFunctionSpace(mesh, "CG", deg)
+K = FunctionSpace(mesh, "CG", deg)
+Xs = Function(V)
 
-mesh = RectangleMesh(nx, ny, Lx, Ly)
+Xn.assign(Xn*(1.0+Xn[0])**2)
 
-V = VectorFunctionSpace(mesh, "CG", 2, dim=3)
-Q = FunctionSpace(mesh, "CG", 1)
-M = MixedFunctionSpace((V,Q))
-w = Function(M)
+mesh.init_cell_orientations(Xn)
+nu = TestFunction(V)
 
-dX, kappa = split(w)
+W = MixedFunctionSpace((V,K))
+w = Function(W)
+Xnp, kappa = split(w)
+eta, chi = TestFunctions(W)
 
-x0 = mesh.coordinates
-X0 = Function(VX)
-X0.interpolate(as_vector([x0[0], x0[1], 0.]))
-
-X = X0 + dX
-
-nu, chi = TestFunctions(M)
-
-# d/dX = (dX/dx)^{-1}d/dx
-J = grad(X)
-
-Jinv = dot(inv(dot(J.T,J)),J.T)
-def Xgrad(q):
-    return dot(Jinv.T, grad(q).T)
-
-j1 = as_vector([X[0].dx(0),X[1].dx(0),X[2].dx(0)])
-j2 = as_vector([X[0].dx(1),X[1].dx(1),X[2].dx(1)])
-n_mag = cross(j1,j2)
-n = cross(j1,j2)/(dot(n_mag,n_mag)**0.5)
-
-#need to define a normal
-#need to include a change of measure
+nu = CellNormal(mesh)
+Dt = 0.01
+dt = Constant(Dt)
 
 F = (
-    inner(Xgrad(X), Xgrad(nu))
+    inner(Xnp - Xn, chi*nu) - dt*kappa*chi +
+    inner(kappa*nu, eta) + inner(grad(Xnp), grad(eta))
     )*dx
+
+prob = NonlinearVariationalProblem(F, w)
+solver = NonlinearVariationalSolver(prob,
+                                    solver_parameters=
+                                    {'mat_type': 'aij',
+                                     'snes_linesearch_type':'basic',
+                                     "snes_monitor":True,
+                                     'ksp_type': 'preonly',
+                                     'pc_type': 'lu'})
+
+T = 10.
+t = 0.
+
+file = File('curvatureflow.pvd')
+
+while t < T - Dt/2:
+    solver.solve()
+
+    X_out, kappa_out = w.split()
+    mesh.coordinates.assign(X_out)
+    Xs.assign(X_out)
+    file.write(Xs)
